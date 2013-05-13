4e183530cb24efdb11d41c93bd55459003742e43
Deepak Kannan <kannan.deepak@gmail.com>
2013-05-13 13:16:26 +0530

    edit Raketask for minitest
    minitest rake task has the test folder in the load-path
    require debugger by default for ease-of-use
    require 'pp' ie. pretty-print library for easy one-off debugging prints

--- test/socialite_js/source_test.rb
+++ test/socialite_js/source_test.rb
@@ -1,4 +1,4 @@
-require_relative '../test_helper'
+require 'test_helper'
 require 'socialite_js/source'
 
 module SocialiteJs
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'minitest/spec'
 require 'minitest/pride'
 require 'minitest/autorun'
+require 'pp' # pretty print for one-off debugging in tests
